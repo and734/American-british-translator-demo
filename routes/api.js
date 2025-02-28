68e146aeddbddf9be5cd5ea74cf3a4761d6e709e
and734 <123209542+and734@users.noreply.github.com>
2025-02-28 21:44:13 +0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,28 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const { text, locale } = req.body;
+
+      //error handle
+      if(text === undefined || !locale){
+        return res.json({error: 'Required field(s) missing'});
+      }
+      if(text === ""){
+        return res.json({error: 'No text to translate'});
+      }
+
+      const result = translator.translate(text, locale);
+
+      //error handle
+      if(result.error){
+        return res.json(result)
+      }
+
+      res.json(result);
     });
 };
